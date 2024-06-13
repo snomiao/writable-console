8dc73b9ea2ebc2da41e4bbdba5449b0754759474
snomiao <snomiao@gmail.com>
2024-06-13 06:28:26 +0000

    fix(main): add tset
    add tset

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Simple Writable Stream console.write, works both text chunks and UInt8Array
 
 ```ts
+
 export const WritableConsole = () => [
   new WritableStream({
     write: (chunk) => {
--- index.test.ts
+++ index.test.ts
@@ -2,15 +2,21 @@ import { WritableConsole } from ".";
 import TextDecoderStream from "polyfill-text-decoder-stream";
 import TextEncoderStream from "polyfill-text-encoder-stream";
 
-await new Response("hello, world (uint8)\n").body?.pipeTo(
-  new WritableConsole()
-);
+it("should pipe into console", async () => {
+  console.write = jest.fn();
 
-await new Response("hello, world (text)\n").body
-  ?.pipeThrough(new TextDecoderStream("utf-8")) // UInt8Array stream into string stream
-  ?.pipeTo(new WritableConsole());
+  await new Response("hello, world (uint8)\n").body?.pipeTo(
+    new WritableConsole()
+  );
 
-await new Response("hello, world (uint8) again\n").body
-  ?.pipeThrough(new TextDecoderStream("utf-8")) // UInt8Array stream into string stream
-  ?.pipeThrough(new TextEncoderStream()) // UInt8Array stream into string stream
-  ?.pipeTo(new WritableConsole());
+  await new Response("hello, world (text)\n").body
+    ?.pipeThrough(new TextDecoderStream("utf-8")) // UInt8Array stream into string stream
+    ?.pipeTo(new WritableConsole());
+
+  await new Response("hello, world (uint8) again\n").body
+    ?.pipeThrough(new TextDecoderStream("utf-8")) // UInt8Array stream into string stream
+    ?.pipeThrough(new TextEncoderStream()) // UInt8Array stream into string stream
+    ?.pipeTo(new WritableConsole());
+
+  expect(console.write).toHaveBeenCalledTimes(3);
+});
